f2323e4e2f204418f7686c9433dae0f4786b2426
nikBaben <bikita.nabkin@gmail.com>
2026-04-30 13:32:45 +0300

    feat: add repository entity

---
+++ domain/entities/repository.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class Repository(BaseModel):
+    """Модель сущности репозитория"""
+    name: str
+    owner_login: str
+    stars: int
+    forks: int
+    watchers: int
+    open_issues: int
+    archived: bool
+    pushed_at: datetime 
