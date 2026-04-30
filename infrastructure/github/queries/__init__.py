2ad8bfc1e97f7aa716441eae0db3c34a3be6a6d1
nikBaben <bikita.nabkin@gmail.com>
2026-04-30 17:08:01 +0300

    refactor: expose query classes via package __init__

---
+++ infrastructure/github/queries/__init__.py
@@ -0,0 +1,7 @@
+from .commits import CommitsQuery
+from .contributors import ContributorsQuery
+from .forks import ForksQuery
+from .issues import IssuesQuery
+from .pulls import PullsQuery
+from .stars import StarsQuery
+from .base import GitHubPaginatedQuery, default_github_headers
