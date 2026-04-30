8e71b23d0ff31d6903a469d2283811ec4d575250
nikBaben <bikita.nabkin@gmail.com>
2026-04-30 18:25:42 +0300

    feat: add port HistoryStoragePort for cache

---
+++ application/ports/repository_history_storage_port.py
@@ -0,0 +1,16 @@
+from typing import Protocol
+
+from application.dto import CachedHistoryDTO
+from application.queries.queries import GetHistoryQuery
+
+
+class HistoryStoragePort(Protocol):
+    """Порт для работы с хранилищем кэша исторических данных репозитория."""
+    
+    def save(self, query: GetHistoryQuery, cached: CachedHistoryDTO) -> None:
+        """Сохраняет кэшированные данные для заданного запроса."""
+        ...
+
+    def load(self, query: GetHistoryQuery) -> CachedHistoryDTO | None:
+        """Загружает кэшированные данные для заданного запроса."""
+        ...
