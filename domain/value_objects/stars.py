aea467f4d7e74fff856925f98ed2b891030ad6bd
nikBaben <bikita.nabkin@gmail.com>
2026-04-30 13:35:34 +0300

    feat: add value objects for stars

---
+++ domain/value_objects/stars.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from pydantic import (
+    BaseModel, 
+    ConfigDict,
+    Field
+)
+
+
+class Stars(BaseModel): 
+    """Модель объекта-значения звезд."""
+    stars: int = Field(ge=0)
+
+
+class StarsHistoryPoint(BaseModel):
+    """Модель объекта-значения для истории звезд."""
+    starred_at: datetime
+
+    model_config = ConfigDict(frozen=True)
+
+
+
+
+
+
