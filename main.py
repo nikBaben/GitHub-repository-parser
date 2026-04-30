1430fa14800c510d5d01fa893b086d40f4cf4e4d
nikBaben <bikita.nabkin@gmail.com>
2026-04-30 19:02:48 +0300

    feat: add main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import asyncio
+
+from infrastructure.config import settings
+from infrastructure.github.clients.client import GitHubClient
+from infrastructure.di.container import AppContainer
+from application.queries.queries import GetHistoryQuery
+
+
+async def main() -> None:
+    async with GitHubClient(settings.TOKEN) as github_client:
+        container = AppContainer(github_client)
+
+        get_history_use_case = container.get_history_use_case()
+
+        query = GetHistoryQuery(
+            url=settings.GITHUB_URL,
+            days=settings.DAYS
+        )
+
+        history = await get_history_use_case.execute(query)
+
+        print("=== HISTORY ===")
+        print(history.model_dump(mode="json"))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
