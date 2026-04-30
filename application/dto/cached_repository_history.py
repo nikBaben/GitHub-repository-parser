756b30277e7e327b41bbf001328980e78194685b
nikBaben <bikita.nabkin@gmail.com>
2026-04-30 19:01:26 +0300

    feat: add DTO cache repository history

---
+++ application/dto/cached_repository_history.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+from dto import RepositoryHistoryDTO
+
+
+class CachedHistoryDTO(BaseModel):
+    """
+    DTO для хранения исторических данных репозитория в кэше.
+    
+    Оборачивает RepositoryHistoryDTO, добавляя метаинформацию
+    о времени кэшировани.
+    """
+    cached_at: datetime
+    history: RepositoryHistoryDTO
