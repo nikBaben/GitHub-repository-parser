dd7ba52a4663b45e62d11973c9b24f084ca256ed
nikBaben <bikita.nabkin@gmail.com>
2026-05-04 14:06:44 +0300

    feat: add score entitie

---
+++ domain/entities/repository_score.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel
+
+from domain.value_objects import (
+    Archived,
+    Commits,
+    Contributors,
+    Forks,
+    Issues,
+    OpenIssues,
+    LastPush,
+    MergedPulls,
+    Pulls,
+    Stars,
+    Watchers,
+    PopularityScore,
+    ActivityScore,
+    EngagementScore,
+    DemandScore
+)
+
+
+class RepositoryScore(BaseModel):
+    """Модель, представляющая полный скоринг репозитория"""
+    archived: Archived
+    commits: Commits
+    contributors: Contributors
+    forks: Forks
+    issues: Issues
+    open_issues: OpenIssues
+    last_push: LastPush
+    pulls: Pulls
+    merged_pulls: MergedPulls
+    stars: Stars
+    watchers: Watchers
+    popularity_score: PopularityScore
+    activity_score: ActivityScore
+    engagement_score: EngagementScore
+    demand_score: DemandScore
